da5834f80d58ae5251b87ec2c108cb47a691f8cd
ill_Nino <innxfect@gmail.com>
2024-08-26 14:12:10 +0300

    feat: add pagination (Mon, Aug 26, 2024 02:12:10 PM)

--- index.js
+++ index.js
@@ -1,54 +1,78 @@
 const sliderImages = document.querySelectorAll('.img__meme');
-const sliderLine = document.querySelector('.slider-container');
+const sliderContainer = document.querySelector('.slider-container');
 const sliderDots = document.querySelectorAll('.pagination__dot');
 const sliderTitle = document.querySelectorAll('.title');
-const sliderDescription = document.querySelector('.description');
+const sliderDescription = document.querySelector('.description-container');
 
 let sliderCount = 0;
 let sliderWidth;
+let titleCount = 0;
+let titleWidth;
 
-window.addEventListener('resize', showSlide);
+
+
 
 function showSlide() {
     sliderWidth = document.querySelector('.slider').offsetWidth;
-    sliderLine.style.width = sliderWidth * sliderImages.length + 'px';
-    sliderImages.forEach(item => item.style.width = sliderWidth + 'px')
+    sliderContainer.style.width = sliderWidth * sliderImages.length + 'px';
+    sliderImages.forEach(item => item.style.width = sliderWidth + 'px');
+    rollSlider();
 }
 
+window.addEventListener('resize', showSlide);
 showSlide();
+
+
+function showTitle() {
+    sliderWidth = document.querySelector('.slider').offsetWidth;
+    sliderDescription.style.width = sliderWidth * sliderTitle.length + 'px';
+    sliderTitle.forEach(title => title.style.width = sliderWidth + 'px');
+    rollSlider();
+}
+
+window.addEventListener('resize', showTitle);
+showTitle();
 
 function nextSlide() {
     sliderCount++;
     if(sliderCount >=sliderImages.length) sliderCount = 0;
+    if(sliderCount >=sliderTitle.length) titleCount = 0;
     rollSlider();
     thisSlide(sliderCount);
+    thisTitle(titleCount);
 }
 
 function prevSlide() {
     sliderCount--;
     if (sliderCount < 0) sliderCount = sliderImages.length - 1;
+    if (sliderCount < 0) sliderCount = sliderTitle.length - 1;
     rollSlider();
     thisSlide(sliderCount);
+    thisTitle(titleCount);
 }
 
 function rollSlider () {
-    sliderLine.style.transform = `translateX(${-sliderCount * sliderWidth}px)`;
+    sliderContainer.style.transform = `translateX(${-sliderCount * sliderWidth}px)`;
+    sliderDescription.style.transform = `translateX(${-titleCount * sliderWidth}px)`;
 }
+
 
 function thisSlide(i) {
     sliderDots.forEach(item => item.classList.remove('active__dot'));
-    sliderDots[i].classList.add('.active__dot');
+    sliderDots[i].classList.add('active__dot');
 }
 
-function newDescription() {
-    const currentCard = sliderImages[i];
-    sliderTitle.textContent = currentCard.getAttribute('data-text');
+function thisTitle(i) {
+    sliderTitle.forEach(text => text.classList.remove('active__txt'));
+    sliderTitle[i].classList.add('active__txt');
 }
 
 sliderDots.forEach((dot, i) => {
     dot.addEventListener('click', () => {
         sliderCount = i;
+        titleCount = i;
         rollSlider();
+        thisTitle(titleCount);
         thisSlide(sliderCount);
     })
-})
+});
